637b1877aa27ad07123be36bea1855786117e799
FarzanaHaider <farzanahaider1425@gmail.com>
2024-03-27 02:02:30 +0600

    27/3/2024

---
+++ 60.dictionary comprehension.py
@@ -0,0 +1,4 @@
+# dictionary comprehension = create dictionaries using an expression
+#                            can replace for loops and certain lambda functions
+#
+# dictionary = {key: expression for (key,value) in iterable}
